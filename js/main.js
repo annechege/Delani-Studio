cbd16eb280a079f3a5ec592a7244df037bf236d3
annechege <annechege780@gmail.com>
2021-04-17 14:18:51 +0300

    adding photos to the what we do section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-$(document.)
+$(document).ready(function() {
+    var elem = this;
+    $('.icon-holder.one').click(function (){
+        $('.one').hide()
+        $('.content-one').show();
+    })
+    $('.icon-holder.two').click(function (){
+        $('.two').hide();
+        $('.content-two').show();
+    })
+    $('.icon-holder.three').click(function (){
+        $('.three').hide();
+        $('.content-three').show();
+    })
+});
