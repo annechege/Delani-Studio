993cc7d58e78a06282a99d88aff7da83333e2c69
annechege <annechege780@gmail.com>
2021-04-19 12:00:16 +0300

    creating the hide and show functions on the js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    var elem = this;
     $('.icon-holder.one').click(function (){
         $('.one').hide()
         $('.content-one').show();
@@ -11,5 +10,17 @@ $(document).ready(function() {
     $('.icon-holder.three').click(function (){
         $('.three').hide();
         $('.content-three').show();
+    })
+    $('.icon-content.content-one').click(function (){
+        $('.content-one').hide()
+        $('.one').show();
+    })
+    $('.icon-content.content-two').click(function (){
+        $('.content-two').hide()
+        $('.two').show();
+    })
+    $('.icon-content.content-three').click(function (){
+        $('.content-three').hide()
+        $('.three').show();
     })
 });
